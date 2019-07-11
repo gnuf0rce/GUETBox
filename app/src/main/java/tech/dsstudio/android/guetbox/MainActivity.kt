c82ec440058dc27b18b32769c29acf5d94f9cc52
dousha <lijiahao34@gmail.com>
2019-07-12 00:28:59 +0800

    init

---
+++ app/src/main/java/tech/dsstudio/android/guetbox/MainActivity.kt
@@ -0,0 +1,12 @@
+package tech.dsstudio.android.guetbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
